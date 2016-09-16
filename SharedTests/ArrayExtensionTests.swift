8a5c12acb61d970e1cb36b43b6744de3e494ddd8
farhanpatel <farhanpatel.17@gmail.com>
2016-09-15 19:47:43 -0700

    Fix linting errors

--- SharedTests/ArrayExtensionTests.swift
+++ SharedTests/ArrayExtensionTests.swift
@@ -1,10 +1,7 @@
-//
-//  ArrayExtensionTests.swift
-//  Client
-//
-//  Created by Farhan Patel on 9/15/16.
-//  Copyright © 2016 Mozilla. All rights reserved.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 import Foundation
 import Shared
 import XCTest
@@ -20,34 +17,34 @@ class ArrayExtensionTests: XCTestCase {
     }
 
     func testUnique() {
-        let a = [1,2,3,4,5,6,1,2]
+        let a = [1, 2, 3, 4, 5, 6, 1, 2]
         let result = a.unique { return $0 }
-        XCTAssertEqual(result, [1,2,3,4,5,6])
+        XCTAssertEqual(result, [1, 2, 3, 4, 5, 6])
 
-        let b = [1,2,3]
+        let b = [1, 2, 3]
         let resultB = b.unique { return $0 }
-        XCTAssertEqual(resultB, [1,2,3])
+        XCTAssertEqual(resultB, [1, 2, 3])
     }
 
     func testUnion() {
-        let a = [1,2,3,4,5,6]
-        let b = [7,8,9,10]
-        XCTAssertEqual(a.union(b) { return $0 }, [1,2,3,4,5,6,7,8,9,10])
+        let a = [1, 2, 3, 4, 5, 6]
+        let b = [7, 8, 9, 10]
+        XCTAssertEqual(a.union(b) { return $0 }, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
-        let c = [1,2,3,4,5,6]
-        let d = [4,5,6,7,8,9,10]
-        XCTAssertEqual(c.union(d) { return $0 }, [1,2,3,4,5,6,7,8,9,10])
+        let c = [1, 2, 3, 4, 5, 6]
+        let d = [4, 5, 6, 7, 8, 9, 10]
+        XCTAssertEqual(c.union(d) { return $0 }, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
-        let e = [1,2,3,4,5,6,7,8,9,10]
-        let f = [4,5,6,7,8,9,10]
-        XCTAssertEqual(e.union(f) { return $0 }, [1,2,3,4,5,6,7,8,9,10])
+        let e = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        let f = [4, 5, 6, 7, 8, 9, 10]
+        XCTAssertEqual(e.union(f) { return $0 }, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
-        let g = [1,2,3,4,5,6,7,8,9,10]
+        let g = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         let h = [Int]()
-        XCTAssertEqual(g.union(h) { return $0 }, [1,2,3,4,5,6,7,8,9,10])
+        XCTAssertEqual(g.union(h) { return $0 }, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
         let i = [Int]()
-        let j = [1,2,3,4,5,6,7,8,9,10]
-        XCTAssertEqual(i.union(j) { return $0 }, [1,2,3,4,5,6,7,8,9,10])
+        let j = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        XCTAssertEqual(i.union(j) { return $0 }, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     }
 }
